4476df1131de93651249a1acd6ab43545d23fcb5
Lucas Veras <lucasdsveras@gmail.com>
2019-07-06 23:39:49 +0100

    Minor change

--- code/functions/read_strength_data.R
+++ code/functions/read_strength_data.R
@@ -2,7 +2,7 @@ read_strength_data <- function(file) {
   
   library(tidyverse)
   
-  df <- read_table(file, skip = 6, col_names = FALSE) %>% 
+  df <- read_table(file, skip = 6, col_names = FALSE, col_types = cols()) %>% 
     select(
       time = X1,
       torque = X2,
