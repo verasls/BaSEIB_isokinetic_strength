328a8cc5a3452952c262151ddb17508622dc4dc7
Lucas Veras <lucasdsveras@gmail.com>
2019-07-06 10:19:23 +0100

    Initial commit, read_strength_data() function

---
+++ code/functions/read_strength_data.R
@@ -0,0 +1,15 @@
+read_strength_data <- function(file) {
+  
+  library(tidyverse)
+  
+  df <- read_table(file, skip = 6, col_names = FALSE) %>% 
+    select(
+      time = X1,
+      torque = X2,
+      position = X3,
+      anat_position = X4,
+      velocity = X5
+    ) %>% # criar data dictionary
+    mutate(abs_torque = abs(torque)) %>% 
+    select(time, torque, abs_torque, position, anat_position, velocity) 
+}
