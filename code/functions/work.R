6e88480efbe4445c3e0c058f377a95042ce9fe05
Lucas Veras <lucasdsveras@gmail.com>
2019-07-06 09:44:31 +0100

    Change return to absolute work value; cosmetic changes

--- code/functions/work.R
+++ code/functions/work.R
@@ -1,15 +1,14 @@
-Work_Integration <- function(filename) 
-{
-  # File reading
-  M <- as.matrix(read.csv(filename)) # Reads file as matrix	
+work <- function(df) {
+  
+  M <- as.matrix(df)
   n <- dim(M)[1] - 1 # Gets the number of intervals in the matrix
+  
   # Numerical integration
-  sum <- 0.0 # Initializes sum variable to zero
-  for (i in 1:n) # For each of the n integration intervals	
-  {				
-    sum = sum - M[i,2]*M[i,3] - M[i+1,2]*M[i,3] + M[i,2]*M[i+1,3] + M[i+1,2]*M[i+1,3];			
+  sum <- 0.0
+  for (i in 1:n) {				
+    sum <- sum - M[i, 2] * M[i, 3] - M[i + 1, 2] * M[i, 3] + M[i, 2] * M[i + 1, 3] + M[i + 1, 2] * M[i + 1, 3]
   }
-  sum = pi*sum/360.0 # Correcting value
-  # Returning calculated value without a name
-  return(unname(sum))	
+  sum <- pi * sum / 360.0 # Correcting value
+
+  return(unname(abs(sum)))
 }
