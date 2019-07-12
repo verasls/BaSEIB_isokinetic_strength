2745f9626d1eeb0a11a09d6e4059cb3662948b48
Lucas Veras <lucasdsveras@gmail.com>
2019-07-12 22:09:42 +0100

    Put power in absolute values and correct conversion from degrees to radians in max power computation

--- code/functions/compute_power.R
+++ code/functions/compute_power.R
@@ -3,11 +3,11 @@ compute_power <- function(df) {
   M <- as.matrix(df)
 	n <- dim(M)[1] - 1 
 	
-	max <- M[1, 2] * M[1, 5] # Maximum value gets initialized with first power value
+	max <- abs(M[1, 2] * M[1, 5]) # Maximum value gets initialized with first power value
 	avg <- max # Avarage starts as a summation of all values
 	for (i in 2:n) # For each of the n integration intervals minus the first, which was already computed	
 	{	
-		value <- M[i, 2] * M[i, 5] # Power value for this iteration			
+		value <- abs(M[i, 2] * M[i, 5]) # Power value for this iteration			
 		avg = avg + value # Add value to avarage calculation 
 		if (value > max) # If this power value is larger than the stored maximum, it becomes the new maximum
 		{
@@ -16,7 +16,7 @@ compute_power <- function(df) {
 
 	}
 	avg = pi * (avg / n) / 180 # Averaging and changing from degrees to radians
-	max = pi * max / 1800.0 # Changing from degrees to radians
+	max = pi * max / 180 # Changing from degrees to radians
 	# Returning max and avarega
 	vec <- c(max, avg)
 	return(unname(vec))	
