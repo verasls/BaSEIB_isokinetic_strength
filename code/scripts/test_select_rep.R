a686a56956fa977ab159df9378f1f46eb3f1f032
Lucas Veras <lucasdsveras@gmail.com>
2019-07-19 15:41:41 -0300

    Visualize divisions of all IDs from all evals

--- code/scripts/test_select_rep.R
+++ code/scripts/test_select_rep.R
@@ -1,88 +1,15 @@
 # Load packages and functions ---------------------------------------------
 
 library(tidyverse)
-source("code/functions/read_strength_data.R")
+source("code/functions/plot_divisions.R")
 
-# Read data ---------------------------------------------------------------
 
-test <- read_strength_data("data/raw/knee/60gs/1st_eval/1st_strength_knee_raw_60g_001.txt")
+# Plot divisions for all evals --------------------------------------------
 
-M <- as.matrix(test)
-
-# Ensure 1st velocity value to be positive
-if (M[1, 5] <= 0) {
-  # Find first positive velocity value
-  p <- min(which(M[, 5] > 0))
-  M <- M[p:nrow(M), ]
-}
-
-# Find zero crossings in velocity signal
-idx <- vector() # Division points indices
-j   <- 1        # Index counter
-for (i in 2:nrow(M)) {
-  if (M[i - 1, 5] * M[i, 5] < 0) { # If product < 0, it means different signs
-    idx[j] <- i
-    j <- j + 1
-  } else {
-    if (M[i - 1, 5] * M[i, 5] == 0) { # If product is 0, at least one of the velocity values is 0
-                                      # Mark where velocity is 0 as index
-      if (min(which(M[(i - 1):i, 5] == 0)) == 1) {
-        idx[j] <- i - 1
-        j <- j + 1
-      } else {
-        if (min(which(M[(i - 1):i, 5] == 0)) == 2) {
-          idx[j] <- i
-          j <- j + 1
-        }
-      }
-    }
-  }
-  idx <- unique(idx[!is.na(idx)])
-}
-
-# Keep only first index value where indices are consecutive
-c <- vector() # Vector indicating where idx values are consecutive
-k <- 1        # Index counter
-for (i in 2:length(idx)) {
-  if (idx[i] - idx[i - 1] == 1) {
-    c[k] <- i
-    k <- k + 1
+evals <- c("1st_eval/", "2nd_eval/", "3rd_eval/", "4th_eval/")
+for (e in evals) {
+  files_60gs <- list.files(str_c("data/raw/knee/60gs/", e), full.names = TRUE)
+  for (i in 1:length(files_60gs)) {
+    plot_divisions(files_60gs[i], show = FALSE)
   }
 }
-if (length(c) != 0) {
-  idx <- idx[- c]
-}
-
-# Ensure that idx where velocity is 0 are followed by a sign change
-p <- vector()
-l <- 1
-for (i in 1:length(idx)) { # identify where idx points to velocity 0
-  if (M[idx[i], 5] == 0) {
-    p[l] <- i
-    l <- l + 1
-  }
-}
-if (length(p) > 0) {
-  for (i in 1:length(p)) {
-    n <- min(which(M[idx[p[i]]:nrow(M), 5] != 0)) + idx[p[i]] - 1 # Find first value after 0
-    if (M[idx[p[i]] - 1, 5] * M[n, 5] > 0) { # If product > 0, it means no sign change
-      idx[i] <- NA
-    }
-  }
-  idx <- idx[!is.na(idx)]
-}
-
-# Find time points of velocity zero crossings
-t <- vector()
-for (i in 1:length(idx)) {
-  t[i] <- M[idx[i], 1]
-}
-
-# Torque x time plot with half-repetition divisions
-ggplot(data = as_tibble(M)) +
-  geom_line(mapping = aes(x = time, y = torque), colour = "blue") +
-  geom_vline(xintercept = t) +
-  labs(
-    x = "Time (ms)",
-    y = expression(Torque~(N*"\U00B7"*m))
-  ) # put ID and eval as plot title
