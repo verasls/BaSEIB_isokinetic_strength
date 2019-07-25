999729e58e379ebacd16c29d0c428e734d2b002f
Lucas Veras <lucasdsveras@gmail.com>
2019-07-25 20:03:43 -0300

    Initial commit; fle_quality_control() function

---
+++ code/functions/fle_quality_control.R
@@ -0,0 +1,45 @@
+fle_quality_control <- function(file) {
+  # Run a quality control test on flexion repetition files to assess whether
+  # torque signs, velocity signs and anatomic position angles are correct and
+  # apply write_log() function to write this information on a txt file
+  #
+  # Args:
+  #   file: name of the file containing isokinetic strength test data for a
+  #   flexion repetition
+  #
+  # Returns:
+  #   A txt file containing the information of in which rows the quality control
+  #   fails
+  
+  M <- as.matrix(read.delim(file, sep = " "))
+  
+  # Detect positive torque and velocity values during knee flexion
+  t <- vector()
+  a <- 1
+  v <- vector()
+  b <- 1
+  for (i in 1:nrow(M)) {
+    # Torque
+    if (M[i, 2] > 0) {
+      t[a] <- i
+      a <- a + 1
+    }
+    # Velocity
+    if (M[i, 5] > 0) {
+      v[b] <- i
+      b <- b + 1
+    }
+  }
+  
+  # Check wheter anatomic position angles decrease
+  p <- vector()
+  c <- 1
+  for (i in 2:nrow(M)) {
+    if (M[i, 4] < M[i - 1, 4]) {
+      p[c] <- i
+      c <- c + 1
+    }
+  }
+  
+  write_log(file, t, v, p)
+}
