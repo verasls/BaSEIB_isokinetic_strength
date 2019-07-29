19469b49c8edfa5be9ec3301568bdded450cdec0
Lucas Veras <lucasdsveras@gmail.com>
2019-07-29 16:45:05 -0300

    Initial commit; quality_control() function

---
+++ code/functions/quality_control.R
@@ -0,0 +1,23 @@
+quality_control <- function(file) {
+  # Detect whether a file is for extension or flextion and apply the correct
+  # quality control function to it
+  #
+  # Args:
+  #   file: name of the file containing isokinetic strength test data
+  #
+  # Returns:
+  #   A txt file containing the information of in which rows the quality control
+  #   fails
+  
+  require(stringr)
+  source("code/functions/ext_quality_control.R")
+  source("code/functions/fle_quality_control.R")
+  
+  if (str_detect(file, "ext")) {
+    ext_quality_control(file)
+  } else {
+    if (str_detect(file, "fle")) {
+      fle_quality_control(file)
+    }
+  }
+}
