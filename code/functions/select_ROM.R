1abd64144fb0935aaa43904d4ee7fa2cd6b57018
Lucas Veras <lucasdsveras@gmail.com>
2019-07-30 19:10:48 -0300

    Initial commit; select_ROM() function

---
+++ code/functions/select_ROM.R
@@ -0,0 +1,17 @@
+select_ROM <- function(file, ROM) {
+  # Select a range of motion on the isokinetic strength test data
+  #
+  # Args:
+  #   file: name of the file containing isokinetic strength test data
+  #   ROM: a numeric sequence corresponding the desired range of motion
+  #
+  # Returns:
+  #   The isokinetic strength test data on the selected range of motion as a
+  #   matrix
+  
+  M <- as.matrix(read.delim(file, sep = " "))
+  
+  M <- M[which(M[, 4] %in% ROM), ]
+  
+  return(M)
+}
