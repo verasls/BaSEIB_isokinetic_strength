9c8721f46ceea40ae8e1840a820400b246a9478b
Lucas Veras <lucasdsveras@gmail.com>
2019-08-09 21:03:25 -0300

    Update variables computation

--- code/scripts/test_calc_variables.R
+++ code/scripts/test_calc_variables.R
@@ -1,15 +1,31 @@
-# Load packages
-library(tidyverse)
-source("code/functions/work.R")
+source("code/functions/select_ROM.R")
+source("code/functions/work_integration.R")
+source("code/functions/compute_power.R")
 
-# Read data
-ext <- read_csv("data/4th_055_knee_60gs_ext.csv")
-flx <- read_csv("data/4th_055_knee_60gs_flx.csv")
+# read body_composition file
+B <- read.csv("data/raw/body_composition.csv")
 
-# Compute variables
-peak_torque <- max(ext$abs.Torque_NM)
-peak_torque_BW <- peak_torque / 59.5 # subject body weight (kg)
-time_peak_torque <- ext$Time_mSec[which(ext$abs.Torque_NM == peak_torque)] - ext$Time_mSec[1]
-angle_peak_torque <- ext$POS_Anat_Degrees[which(ext$abs.Torque_NM == peak_torque)]
-torque_0.18seg <- ext$Torque_NM[which(ext$Time_mSec == (ext$Time_mSec[1] + 180))] # needs some adjustment 
-work <- work(ext)
+# read isokinetic strength test file
+file <- "data/processed/knee/60gs/1st_eval/separate_reps/1st_strength_knee_60g_001_ext_1.txt"
+
+M <- select_ROM(file, 10:80)
+
+# desired variables
+
+peak_torque <- max(abs(M[, 2]))
+peak_torque_BW <- peak_torque / B[1, 2]
+peak_torque_LW <- peak_torque / B[1, 4]
+peak_torque_angle <- unname(M[which(M[, 2] == peak_torque), 4])
+total_work <- work_integration(M)
+average_power <- compute_power(M)[2]
+peak_power <- compute_power(M)[1]
+
+# the following variables need to be computed after all reps are done
+
+max_peak_torque_rep
+max_peak_torque_BW_rep
+max_peak_torque_LW_rep
+max_peak_torque_angle_rep
+max_total_work_rep
+max_average_power_rep
+max_peak_power_rep
