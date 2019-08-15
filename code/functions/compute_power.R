257ddda9c64ca6524356e272e2542fc11c74390d
Lucas Veras <lucasdsveras@gmail.com>
2019-08-15 13:17:39 -0300

    Change style

--- code/functions/compute_power.R
+++ code/functions/compute_power.R
@@ -15,16 +15,18 @@ compute_power <- function(df) {
 	for (i in 2:n) # For each of the n integration intervals minus the first, which was already computed	
 	{	
 		value <- abs(M[i, 2] * M[i, 5]) # Power value for this iteration			
-		avg = avg + value # Add value to avarage calculation 
+		avg   <- avg + value # Add value to avarage calculation 
 		if (value > max) # If this power value is larger than the stored maximum, it becomes the new maximum
 		{
-			max = value
+			max <- value
 		}
 
 	}
-	avg = pi * (avg / n) / 180 # Averaging and changing from degrees to radians
-	max = pi * max / 180 # Changing from degrees to radians
+	avg <- pi * (avg / n) / 180 # Averaging and changing from degrees to radians
+	max <- pi * max / 180 # Changing from degrees to radians
+	
 	# Returning max and avarega
 	vec <- c(max, avg)
+	
 	return(unname(vec))	
 }
