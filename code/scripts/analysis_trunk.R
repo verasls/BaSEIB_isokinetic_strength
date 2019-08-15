88782ac1ba0c614b836832faeea7cbb8e2729dff
Lucas Veras <lucasdsveras@gmail.com>
2019-08-15 15:10:21 -0300

    Initial commit

---
+++ code/scripts/analysis_trunk.R
@@ -0,0 +1,24 @@
+# Load packages and functions ---------------------------------------------
+
+library(tidyverse)
+source("code/functions/plot_divisions.R")
+
+# Plot divisions for all evals --------------------------------------------
+
+evals <- c("1st_eval", "2nd_eval", "3rd_eval", "4th_eval")
+
+# 60º/s
+for (i in evals) {
+  files_60gs <- list.files(str_c("data/raw/trunk/60gs/", i), full.names = TRUE)
+  for (i in 1:length(files_60gs)) {
+    plot_divisions(files_60gs[i], show = FALSE)
+  }
+}
+
+# 180º/s
+for (i in evals) {
+  files_120gs <- list.files(str_c("data/raw/trunk/120gs/", i), full.names = TRUE)
+  for (i in 1:length(files_120gs)) {
+    plot_divisions(files_120gs[i], show = FALSE)
+  }
+}
