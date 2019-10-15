2b69c1ca137a494ca5ac68d28166808556b1ca0d
Lucas Veras <lucasdsveras@gmail.com>
2019-10-15 18:32:39 +0100

    Delete unecessary parts of the file

--- analysis_knee.py
+++ analysis_knee.py
@@ -1,6 +1,4 @@
 import glob
-import numpy as np
-import matplotlib.pyplot as plt
 from processing import find_divisions, plot_divisions
 
 files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
@@ -8,20 +6,9 @@ files_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
 files_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
 files_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
 
-# data = np.loadtxt(files_raw_60gs_1st[0], skiprows=6)
-# time = data[:, 0]
-# torque = data[:, 1]
-# 
-# fig = plt.figure()
-# f1 = fig.add_subplot(1, 1, 1)
-# f1.plot(time, torque)
-# f1.set_xlabel("Time (ms)")
-# f1.set_ylabel("Torque (Nm)")
-# plt.show()
-
 idx = find_divisions(files_raw_60gs_1st[0])
-print(idx)
-print(len(idx))
+print("Indexes are:", idx)
+print("The idx vector length is:", len(idx))
+print("The last index is:", idx[len(idx) - 1])
 
-idx_time = plot_divisions(files_raw_60gs_1st[0])
-print(idx_time)
+plot_divisions(files_raw_60gs_1st[0])
