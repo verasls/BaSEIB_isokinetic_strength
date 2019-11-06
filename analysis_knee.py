e7ff565e21714e1b7dc96ad7bcec1e2e15b1e6ad
Lucas Veras <lucasdsveras@gmail.com>
2019-11-06 15:32:34 +0000

    Change module name

--- analysis_knee.py
+++ analysis_knee.py
@@ -1,9 +1,9 @@
 import glob
-from processing import find_divisions, plot_divisions
+from proisostr import find_divisions, plot_divisions
 
 files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
 files_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
 files_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
 files_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
 
-plot_divisions(files_raw_60gs_1st[0], True)
+plot_divisions(files_raw_60gs_1st[0])
