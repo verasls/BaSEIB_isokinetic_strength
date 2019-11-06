f1a603cf7dea128d654754a3332eea3b37d9a504
Lucas Veras <lucasdsveras@gmail.com>
2019-11-06 16:48:41 +0000

    Import and use read_strength_data() function

--- analysis_knee.py
+++ analysis_knee.py
@@ -1,9 +1,10 @@
 import glob
-from proisostr import find_divisions, plot_divisions
+from proisostr import read_strength_data, plot_divisions, find_divisions
 
 files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
 files_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
 files_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
 files_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
 
-plot_divisions(files_raw_60gs_1st[0])
+data = read_strength_data(files_raw_60gs_1st[0])
+plot_divisions(data)
