f37902ffa74f9639cc5bfdced663e601d08fbe62
Lucas Veras <lucasdsveras@gmail.com>
2019-11-11 15:30:18 +0000

    Test new version of plot_divisions()

--- code/Python/01_analysis_knee.py
+++ code/Python/01_analysis_knee.py
@@ -6,5 +6,5 @@ files_raw_60gs_2nd = sorted(glob.glob("../../data/raw/knee/60gs/2nd_eval/*.txt")
 files_raw_60gs_3rd = sorted(glob.glob("../../data/raw/knee/60gs/3rd_eval/*.txt"))
 files_raw_60gs_4th = sorted(glob.glob("../../data/raw/knee/60gs/4th_eval/*.txt"))
 
-data = read_strength_data(files_raw_60gs_1st[0])
-plot_divisions(data)
+data = read_strength_data(files_raw_60gs_1st[2])
+idx = plot_divisions(data)
