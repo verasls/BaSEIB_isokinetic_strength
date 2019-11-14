ec32ea9a2ae10195da11a80e38c1aa0530e843b4
Lucas Veras <lucasdsveras@gmail.com>
2019-11-14 16:43:52 +0000

    Update module name

--- code/Python/01_analysis_knee.py
+++ code/Python/01_analysis_knee.py
@@ -1,5 +1,5 @@
 import glob
-from proikd import read_strength_data, plot_divisions
+from proiks import read_strength_data, plot_divisions
 
 files_raw_60gs_1st = sorted(glob.glob("../../data/raw/knee/60gs/1st_eval/*.txt"))
 files_raw_60gs_2nd = sorted(glob.glob("../../data/raw/knee/60gs/2nd_eval/*.txt"))
