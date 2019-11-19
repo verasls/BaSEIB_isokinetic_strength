e1f5336ebe326848780ad4da10bf7b601f83f6eb
Lucas Veras <lucasdsveras@gmail.com>
2019-11-19 19:13:06 +0000

    Update code to last version of proiks module

--- code/Python/01_analysis_knee.py
+++ code/Python/01_analysis_knee.py
@@ -1,5 +1,5 @@
 import glob
-from proiks import read_strength_data, plot_divisions
+from proiks import *
 
 files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
 files_raw_60gs_2nd = sorted(glob.glob("/data/raw/knee/60gs/2nd_eval/*.txt"))
@@ -7,4 +7,5 @@ files_raw_60gs_3rd = sorted(glob.glob("/data/raw/knee/60gs/3rd_eval/*.txt"))
 files_raw_60gs_4th = sorted(glob.glob("/data/raw/knee/60gs/4th_eval/*.txt"))
 
 data = read_strength_data(files_raw_60gs_1st[2])
-idx = plot_divisions(data)
+idx = find_divisions(data)
+idx = plot_divisions(data, idx)
