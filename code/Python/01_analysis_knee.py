217a0255873b7c490e865c3c8ba5666f37f828b5
Lucas Veras <lucasdsveras@gmail.com>
2019-11-21 09:00:47 +0000

    Correct path to files

--- code/Python/01_analysis_knee.py
+++ code/Python/01_analysis_knee.py
@@ -2,9 +2,9 @@ import glob
 from proiks import *
 
 files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
-files_raw_60gs_2nd = sorted(glob.glob("/data/raw/knee/60gs/2nd_eval/*.txt"))
-files_raw_60gs_3rd = sorted(glob.glob("/data/raw/knee/60gs/3rd_eval/*.txt"))
-files_raw_60gs_4th = sorted(glob.glob("/data/raw/knee/60gs/4th_eval/*.txt"))
+files_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
+files_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
+files_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
 
 data = read_strength_data(files_raw_60gs_1st[2])
 idx = find_divisions(data)
