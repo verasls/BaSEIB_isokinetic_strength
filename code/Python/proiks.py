325f997933d8413ae6276f0f6af68efb92b15398
Lucas Veras <lucasdsveras@gmail.com>
2019-11-21 09:32:24 +0000

    Add an argument to choose whether or not to save the plt in plot_divisions()

--- code/Python/proiks.py
+++ code/Python/proiks.py
@@ -33,7 +33,7 @@ def find_divisions(data):
     return(idx)
 
 
-def plot_divisions(data, idx):
+def plot_divisions(data, idx, saveplot=True):
     # Ensure 1st velocity value to be positive
     velocity = data[:, 4]
     if velocity[0] <= 0:
@@ -87,20 +87,21 @@ def plot_divisions(data, idx):
     elif answer == "n":
         print("The division points will not be altered")
         
-        fig2 = plt.figure(figsize=(12, 6))
-        ax21 = fig2.add_subplot(1, 1, 1)
-        ax22 = ax21.twinx()
-        ax21.plot(time, torque, "tab:blue")
-        ax22.plot(time, velocity, "tab:orange")
-        for i in range(0, len(idx_time)):
-            ax21.axvline(x=idx_time[i], color="k", linestyle="dotted")
-        ax22.axhline(y=0, color="k", linestyle="dotted")
-        ax21.set_xlabel("Time (ms)")
-        ax21.set_ylabel("Torque (Nm)", color="tab:blue")
-        ax22.set_ylabel("Velocity (degree/s)", color="tab:orange")
-        plt.title("Code-defined division points")
-        plt.savefig("test.pdf")
-        print("Plot saved")
+        if saveplot is True:
+            fig2 = plt.figure(figsize=(12, 6))
+            ax21 = fig2.add_subplot(1, 1, 1)
+            ax22 = ax21.twinx()
+            ax21.plot(time, torque, "tab:blue")
+            ax22.plot(time, velocity, "tab:orange")
+            for i in range(0, len(idx_time)):
+                ax21.axvline(x=idx_time[i], color="k", linestyle="dotted")
+            ax22.axhline(y=0, color="k", linestyle="dotted")
+            ax21.set_xlabel("Time (ms)")
+            ax21.set_ylabel("Torque (Nm)", color="tab:blue")
+            ax22.set_ylabel("Velocity (degree/s)", color="tab:orange")
+            plt.title("Code-defined division points")
+            plt.savefig("test.pdf")
+            print("Plot saved")
 
     return(idx)
 
