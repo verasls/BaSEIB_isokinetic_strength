01014d28de8630ee563292627598b57a789d8bb4
Lucas Veras <lucasdsveras@gmail.com>
2019-11-21 17:44:31 +0000

    Get path to all raw files

--- code/Python/01_analysis_knee.py
+++ code/Python/01_analysis_knee.py
@@ -1,10 +1,27 @@
 import glob
 from proiks import *
 
-files_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
-files_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
-files_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
-files_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
+# Get path to raw files
+# Knee 60°/s
+knee_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
+knee_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
+knee_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
+knee_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
+# Knee 180°/s
+knee_raw_180gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
+knee_raw_180gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
+knee_raw_180gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
+knee_raw_180gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
+# Trunk 60°/s
+trunk_raw_60gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
+trunk_raw_60gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
+trunk_raw_60gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
+trunk_raw_60gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
+# Trunk 120°/s
+trunk_raw_120gs_1st = sorted(glob.glob("data/raw/knee/60gs/1st_eval/*.txt"))
+trunk_raw_120gs_2nd = sorted(glob.glob("data/raw/knee/60gs/2nd_eval/*.txt"))
+trunk_raw_120gs_3rd = sorted(glob.glob("data/raw/knee/60gs/3rd_eval/*.txt"))
+trunk_raw_120gs_4th = sorted(glob.glob("data/raw/knee/60gs/4th_eval/*.txt"))
 
-idx = find_divisions(files_raw_60gs_1st[2])
-idx = plot_divisions(files_raw_60gs_1st[2], idx)
+idx = find_divisions(files_knee_raw_60gs_1st[2])
+idx = plot_divisions(files_knee_raw_60gs_1st[2], idx)
