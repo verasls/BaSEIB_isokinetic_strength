41c3c8f2f6a86a11c3747e1f92e3393ee9a77cd9
Lucas Veras <lucasdsveras@gmail.com>
2019-11-25 20:13:00 +0000

    Correct print statement

--- code/Python/01_analysis.py
+++ code/Python/01_analysis.py
@@ -147,10 +147,10 @@ for i in range(0, len(files)):
 print("\nStarting data analysis...")
 
 for i in range(first_ID_number, len(files)):
-    print("\nReading file ", i, " out of ", len(files), ": ", files[i])
+    print("\nReading file", i + 1, "out of", len(files), ":", files[i])
 
     idx = find_divisions(files[i])
-    idx = plot_divisions(files[i], idx, saveplot=True)
+    idx = plot_divisions(files[i], idx)
 
     continues = input("\nDo you want to continue analysing the other "
                       "files in this directory? (y/n)\n")
